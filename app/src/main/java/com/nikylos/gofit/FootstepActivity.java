40a8e24e30fa13a4326c2e30f8c1a1e6ec935649
Dantard <dantardleo@gmail.com>
2016-12-23 20:13:32 +0800

    Dantard's Update -Change background path to mipmap/asset_background

--- app/src/main/java/com/nikylos/gofit/FootstepActivity.java
+++ app/src/main/java/com/nikylos/gofit/FootstepActivity.java
@@ -1,13 +1,60 @@
 package com.nikylos.gofit;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FootstepActivity extends AppCompatActivity {
+    SensorManager sensorManager;
+    TextView tv_steps;
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_footstep);
+        setContentView(R.layout.activity_main);
+
+        tv_steps = (TextView)findViewById(R.id.tv_steps);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }else{
+            Toast.makeText(this, "Sensor ot found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        //if unregister the hardware will stop detecting count
+        //sensorManager.unregisterListener(this);
+
+    }
+
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(running){
+            tv_steps.setText(String.valueOf(sensorEvent.values[0]));
+        }
+    }
+
+
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
